e0eb305095bb5075bbc6e1a3dc3c08ad131e81bd
Shashwat Pandey <shashwatpandeym.v@gmail.com>
2021-05-07 14:45:04 +0530

    hello-world

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world!" << endl;
+    cout << "Lorem ipsum";
+    cout << "\nhello world! \n";
+    cout << "Lorem ipsum";
+    return 0;
+}
