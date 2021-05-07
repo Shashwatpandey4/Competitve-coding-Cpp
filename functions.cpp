a31b0e19725941d224fe717e1710bf25fc43de93
Shashwat Pandey <shashwatpandeym.v@gmail.com>
2021-05-07 16:15:42 +0530

    function

---
+++ functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void printsr(); //function prototyping
+
+int main()
+{
+
+    printsr();
+    return 0;
+}
+
+void printsr()
+{
+    cout << "\n its a function output \n";
+}
