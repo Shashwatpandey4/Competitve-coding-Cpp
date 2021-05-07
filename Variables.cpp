3813c070445da6f16a9cda9ffcfbf98e35f62d75
Shashwat Pandey <shashwatpandeym.v@gmail.com>
2021-05-07 15:38:22 +0530

    variables intro

---
+++ Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 32;
+    cout << age << endl;
+
+    int a = 90;
+    int b = 82;
+
+    int sum = a + b;
+
+    cout << sum;
+
+    return 0;
+}
---
+++ calc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+
+    cout << "enter a number : \n";
+    cin >> a;
+    cout << "enter another number :\n";
+    cin >> b;
+    sum = a + b;
+    cout << "sum : " << sum << endl;
+
+    return 0;
+}
