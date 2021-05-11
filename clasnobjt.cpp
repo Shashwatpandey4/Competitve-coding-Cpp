9b27f6ac00114b7e0f52923974d13b08748eb95d
Shashwat Pandey <shashwatpandeym.v@gmail.com>
2021-05-11 19:51:09 +0530

    classes & objects

---
+++ clasnobjt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class democlass
+{
+public:
+    void anbc()
+    {
+        cout << "hieowl!!" << endl;
+    }
+};
+
+int main()
+{
+    democlass demoobj;
+    demoobj.anbc();
+    return 0;
+}
